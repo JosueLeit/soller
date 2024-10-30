1348ee480b409efca29ba3a65c263a5129ebb585
JosueLeit <josueleite@outlook.com>
2024-10-30 15:47:11 -0300

    Adjustments on PicktheSun component

--- src/components/PickTheSun.tsx
+++ src/components/PickTheSun.tsx
@@ -4,42 +4,44 @@ import Desktop from '../../public/assets/Desktop.png';
 
 const PickTheSun = () => {
   return (
-    <div className="relative w-full mx-auto px-4 py-12 overflow-hidden">
-    <div className="absolute -left-[220] w-[710] h-[711] bg-amber-500 rounded-full" />
-    <div className="absolute -right-6 bottom-1/4 w-[596] h-[597] bg-purple-700 rounded-full" />
-      <div className="relative justify-items-center z-10">
-            <p className="text-amber-600 text-sm font-medium mb-2">No more waste</p>
-            <h1 className="text-[3.5rem] text-blacked font-extrabold mb-4">Pick the Sun</h1>
-            <p className="text-blacked mb-8 max-w-2xl">
-              Et pulvinar nec pretium integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar
-              purus maecenas quam a. Est porttitor pharetra sed in mauris elementum sollicitudin.
-            </p>
-          <div className="rounded-lg overflow-hidden">
-            <Image
-              src={Desktop}
-              alt="Application interface"
-              width={1520}
-              height={813}
-              className="w-full h-auto"
-            />
-          </div>
+    <section className="relative w-full mx-auto px-4 py-12 overflow-hidden">
+    <div
+      className="absolute rounded-full bg-amber-500 w-[200px] h-[200px]
+      sm:w-[400px] sm:h-[400px]
+      lg:w-[600px] lg:h-[600px]
+      -left-[220] -top-3/3"
+    />
+
+    <div
+      className="absolute rounded-full bg-purple-700 w-[150px] h-[150px]
+      sm:w-[300px] sm:h-[300px]
+      lg:w-[450px] lg:h-[450px]
+       -right-6 bottom-1/4"
+      />
+
+    <div className="relative z-10 max-w-7xl mx-auto text-center">
+      <p className="text-amber-600 text-sm font-medium mb-2">No more waste</p>
+      <h1 className="text-4xl md:text-5xl text-blacked font-extrabold mb-4">
+        Pick the Sun
+      </h1>
+      <p className="text-blacked mb-8 max-w-2xl mx-auto">
+        Et pulvinar nec pretium integer id urna molestie porta nullam. A,
+        donec ornare sed turpis pulvinar purus maecenas quam a. Est porttitor
+        pharetra sed in mauris elementum sollicitudin.
+      </p>
+      
+      <div className="rounded-lg overflow-hidden">
+          <Image
+            src={Desktop}
+            alt="Application interface"
+            width={1520}
+            height={813}
+            className="w-full h-auto"
+          />
+        </div>
       </div>
-  </div>
+    </section>
   )
 }
 
 export default PickTheSun
-
-
-{/* <div className="grid justify-between mt-24">
-      <div className="relative rounded-full w-[710] h-[711] bg-amber-500"></div>
-      <Image 
-        src={Desktop} 
-        alt="Technician working with solar panels" 
-        width={1520} 
-        height={854} 
-        // className="rounded-full  -top-24 absolute z-30"
-        className=""
-      />
-      <div className="relative rounded-full w-[710] h-[711] bg-purple-700"></div>
-    </div> */}
